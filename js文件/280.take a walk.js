33280f85da32103d876f3d0b017897ed0306f25a
xiaolingys <98681393+xiaolingys@users.noreply.github.com>
2022-03-27 00:02:57 +0800

    js文件

---
+++ js文件/280.take a walk.js
@@ -0,0 +1,31 @@
+var time=25;var time1=200;var time2=800;
+var zuobiaoPath = "/sdcard/Download/SkyMsToJs/zuobiao.txt";
+if (files.exists(zuobiaoPath)) {
+    eval(files.read(zuobiaoPath));
+} else {
+    setScreenMetrics(1080, 2340);//默认分辨率，以下按键位置基于此分辨率
+    var x=[780,975,1170,1365,1560];
+    var y=[215,410,605];
+}
+function ran(){return Math.random()*36-18}
+function c4() {press(x[0]+ran(),y[0]+ran(),1)}
+function d4() {press(x[1]+ran(),y[0]+ran(),1)}
+function e4() {press(x[2]+ran(),y[0]+ran(),1)}
+function f4() {press(x[3]+ran(),y[0]+ran(),1)}
+function g4() {press(x[4]+ran(),y[0]+ran(),1)}
+function a4() {press(x[0]+ran(),y[1]+ran(),1)}
+function b4() {press(x[1]+ran(),y[1]+ran(),1)}
+function c5() {press(x[2]+ran(),y[1]+ran(),1)}
+function d5() {press(x[3]+ran(),y[1]+ran(),1)}
+function e5() {press(x[4]+ran(),y[1]+ran(),1)}
+function f5() {press(x[0]+ran(),y[2]+ran(),1)}
+function g5() {press(x[1]+ran(),y[2]+ran(),1)}
+function a5() {press(x[2]+ran(),y[2]+ran(),1)}
+function b5() {press(x[3]+ran(),y[2]+ran(),1)}
+function c6() {press(x[4]+ran(),y[2]+ran(),1)}
+function t1() {sleep(time)}//默认间隔-
+function t2() {sleep(time1)}//较短间隔~
+function t3() {sleep(time2)}//较长间隔,
+function t4() {sleep(100)}//自定义间隔*点一下隔100ms
+f4();t4();t1();a5();t1();t1();a4();t1();t1();f4();t1();a4();t1();g5();t2();t1();t1();t1();a4();t1();e5();t2();t1();t1();t1();a4();t1();d5();t2();t1();t1();t1();a4();t1();e5();t1();t1();t1();a4();t1();a4();t1();g5();t2();t1();t1();t1();e5();t1();e5();t2();t1();t1();t1();a4();t1();d5();t2();t2();t1();f4();t1();a4();a5();t2();t1();t1();t1();a4();t1();g5();t2();t1();t1();t1();a4();t1();e5();t2();t4();d5();t2();t1();t1();t1();t1();e5();t1();t1();t1();a4();t1();g5();t2();t1();t1();a4();t1();e5();t2();t4();f4();d5();t1();a4();t2();t2();t1();f4();t1();a4();d5();t2();t1();t1();a4();t1();c5();t2();t1();t1();t1();f4();t1();d5();t2();t1();t1();t1();g4();t1();a4();e5();t2();t1();t1();t1();a4();t1();d5();t2();t1();t1();t1();a4();t1();c5();t1();t1();t1();a4();t1();d5();t2();t1();t1();t1();f4();t1();a4();c5();t2();t2();t1();t1();t1();a4();t1();g4();t2();t1();t1();t1();c5();t1();a4();t2();t1();t1();t1();f4();t1();d5();t2();t4();g4();a4();t1();e5();t2();t1();t1();t1();f4();d5();t1();a4();t1();t1();c5();t1();a4();t2();t1();t1();t1();a4();t1();f4();t2();t1();t1();t1();c4();t1();g4();t2();t2();t1();f4();t1();a5();a4();t2();t1();t1();t1();g5();t1();a4();t2();t1();t1();t1();a4();e5();t2();t4();f4();a4();t1();d5();t2();t4();a4();e5();t1();t1();t1();c4();t1();f4();f4();g5();t2();t1();t1();t1();g4();t1();a4();e5();e5();t2();t1();t1();t1();c4();t1();f4();d5();t2();t2();t1();t1();c4();t1();f4();a5();t2();t1();t1();t1();t1();g5();t2();t4();a4();t1();e5();t2();t4();t1();a4();d5();t2();t1();t1();t1();e5();t1();t1();t1();g5();t1();a4();t2();t1();t1();t1();a4();t1();e5();t2();t1();t1();a4();t1();d5();t2();t2();t1();t1();t1();f4();a4();t1();d5();t2();t1();t1();a4();t1();c5();t2();t4();f4();d5();t2();t1();t1();t1();g4();e5();t1();e5();t2();t1();t1();a4();t1();d5();t2();t1();t1();t1();a4();t1();c5();t1();t1();t1();a4();t1();d5();t2();t1();t1();t1();a4();t1();c5();t2();t2();t1();f4();t1();a4();d5();t2();t1();t1();t1();c5();t2();t1();t1();t1();d5();t2();t4();e5();t2();t1();t1();t1();d5();t1();t1();t1();c5();t2();t4();a4();t2();t2();t1();t1();t1();a4();t1();a5();t3();
+t2();t4();t1();t1();t1();f4();a4();
